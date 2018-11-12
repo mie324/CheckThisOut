2c8c5a54222728ccf7a99bd4c8f2d8635ca55edb
William <wil.xu@mail.utoronto.ca>
2018-11-12 18:10:56 -0500

    modified main and model code to a working encoder, managed to lower the average angle to pi/4

--- main.py
+++ main.py
@@ -17,6 +17,34 @@ from model import *
 
 spacy_en = spacy.load('en')
 
+def find_angle(target,input):
+    # the input of thie function should be 15000*1, and the output will be 150*1, where the values
+    # are angle between the ith vector in radian
+    vector_size=100
+    vector_array=[]
+    for i in range(int(len(target)/vector_size)):
+        temp_target=[]
+        temp_input=[]
+        for j in range(vector_size):
+            temp_target.append(target[i*vector_size+j].tolist())
+            temp_input.append(input[i*vector_size+j].tolist())
+        temp_target=numpy.asarray(temp_target)
+        norm_target=temp_target/numpy.linalg.norm(temp_target)
+        temp_input=numpy.asarray(temp_input)
+        norm_input=temp_input/numpy.linalg.norm(temp_input)
+        d = numpy.dot(norm_target, norm_input)
+        ans = numpy.arccos(d)
+        vector_array.append(ans/3.14159265358979323846)
+    ave = 0
+    count = 0
+    for k in range(len(vector_array)):
+        if numpy.isnan(vector_array[k])==False:
+            ave += vector_array[k]
+            count+=1
+
+    return vector_array,ave/count
+
+
 def convert_csv_to_tsv(file,new_name):
     # given the "abstract file with 400 abstracts" in the form of csv, it converts the file into tsv, with 400 entries
     file=pd.read_csv(file)
@@ -50,7 +78,7 @@ def adjust_length(sentence):
         sentence=torch.tensor(sentence).long()
         return sentence
     short=[]
-    if len(sentence)>target:
+    if len(sentence)>= target:
         for j in range(target):
             short.append(sentence[j])
         short=torch.tensor(short).long()
@@ -75,9 +103,9 @@ def sentence_preprocess(sentence,Vocab,embeds):
     return word_0
 
 def main():
-    batch_size=64
-    learning_rate=0.001
-    epoch=1
+    batch_size=20
+    learning_rate=0.0001
+    epoch=10
     TEXT = data.Field(sequential=True, include_lengths=True, tokenize='spacy')
     LABEL = data.Field(sequential=False, use_vocab=False)
     abstract_data = data.TabularDataset(path='./data/abstract_tsv.tsv', skip_header=True, format='tsv',fields=[('text',TEXT),('label',LABEL)])
@@ -97,19 +125,26 @@ def main():
 
     # this is the training loop
     net=Autoencoder()
+    # net.cuda()
     optimizer=torch.optim.RMSprop(net.parameters(),lr=learning_rate)
     loss_func=torch.nn.MSELoss()
     file=read_tsv('./data/abstract_tsv.tsv')
+
+    best_loss = 1000
     for i in range(epoch):
         batch_count=0
         validation_amount=400-(int(400/batch_size)-1)*batch_size
         for j in range(int(400/batch_size)-1):
+            print("Epoch#: {}, Batch#: {}".format(i+1,j+1))
             batch_loss=0
             for b in range(batch_size):
                 tsentence = sentence_preprocess(file[batch_count*batch_size+b], Vocab, embeds)
-                prediction=net.forward(tsentence)
+                prediction, encoded = net(tsentence)
                 loss=loss_func(prediction,tsentence)
                 batch_loss+=loss
+                angles,ave = find_angle(tsentence,prediction)
+                print("Average Angle Distance: {}pi".format(ave))
+
             batch_loss=batch_loss/batch_size
             optimizer.zero_grad()
             batch_loss.backward()
@@ -120,12 +155,16 @@ def main():
         # this is for validation at the end of each epoch
         val_loss = 0
         for k in range((int(400/batch_size)-1)*batch_size,400):
-            prediction = net.forward(tsentence)
+            tsentence=sentence_preprocess(file[k],Vocab,embeds)
+            prediction, encoded = net.forward(tsentence)
             loss = loss_func(prediction, tsentence)
             val_loss+=loss
-        val_loss=val_loss/validation_amount
+        val_loss = val_loss/validation_amount
         print('epoch: '+str(i)+', training loss:'+str(batch_loss)+', validation loss:'+str(val_loss))
-        
+        if val_loss < best_loss:
+            torch.save(net, 'model_{}.pt'.format('best'))
+            best_loss = val_loss
+
 
 
 
--- model.py
+++ model.py
@@ -15,19 +15,20 @@ from torch.autograd import Variable
 class Autoencoder(torch.nn.Module):
     def __init__(self):
         super(Autoencoder,self).__init__()
-        self.layer1=nn.Sequential(nn.Linear(15000,8000),nn.Tanh())
-        self.layer2=nn.Sequential(nn.Linear(8000,1000),nn.Tanh())
-        self.layer3 = nn.Sequential(nn.Linear(1000, 100), nn.Tanh())
+        self.layer1 = nn.Sequential(nn.Linear(15000, 800), nn.Tanh())
+        # self.layer2 = nn.Sequential(nn.Linear(9000, 800), nn.Tanh())
+        self.layer3 = nn.Sequential(nn.Linear(800, 50), nn.Tanh())
 
-        self.layer4 = nn.Sequential(nn.Linear(100, 1000), nn.Tanh())
-        self.layer5 = nn.Sequential(nn.Linear(1000, 8000), nn.Tanh())
-        self.layer6 = nn.Sequential(nn.Linear(8000, 15000), nn.Sigmoid())
+        self.layer4 = nn.Sequential(nn.Linear(50, 800), nn.Tanh())
+        #self.layer5 = nn.Sequential(nn.Linear(1000, 8000), nn.Tanh())
+        self.layer6 = nn.Linear(800, 15000)
 
     def forward(self,input):
-        x=self.layer1(input)
-        x=self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.layer5(x)
+
+        x = self.layer1(input)
+        # x = self.layer2(x)
+        encoded = self.layer3(x)
+        x = self.layer4(encoded)
+        #x = self.layer5(x)
         x = self.layer6(x)
-        return x
+        return x, encoded
